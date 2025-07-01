bfeda6223042f30169e48bfa3617271b393a6833
Yuri Ly <yuri@lyubchak.com>
2025-07-01 16:37:49 +0300

    Update fetch_times.json with new timestamps and modify send.sh to include clusterer and ranker config paths

--- send.sh
+++ send.sh
@@ -7,6 +7,8 @@ do
         --client-config-path configs/client_config.json \
         --mongo-config-path configs/mongo_config.json \
         --annotator-config-path configs/annotator_config.json \
+        --clusterer-config-path configs/clusterer_config.json \
+        --ranker-config-path configs/ranker_config.json \
         --renderer-config-path configs/renderer_config.json \
         --daemon-config-path configs/daemon_config.json;
 done
